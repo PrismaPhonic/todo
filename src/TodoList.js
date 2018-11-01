075c3ee663c322d065538d405b1b98788bc9695f
Peter Farr <Peter@PrismaPhonic.com>
2018-11-01 15:14:16 -0700

    Added editable capabilities for todo items

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import Todo from './Todo';
 import TodoForm from './TodoForm';
+import EditTodoForm from './EditTodo';
 import uuid from 'uuid/v4';
 
 class TodoList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      todos: []
+      todos: [],
     };
     this.addTodo = this.addTodo.bind(this);
+    this.saveTodoEdits = this.saveTodoEdits.bind(this);
   }
 
   addTodo(todo) {
@@ -20,8 +22,35 @@ class TodoList extends Component {
   }
 
   removeTodo(id) {
-    this.state.todos.splice(id, 1);
+    let todos = this.state.todos.filter(todo => todo.id !== id);
+    this.setState(st => ({ todos }));
+  }
+
+  makeTodoEditable(id) {
+    for (let todo of this.state.todos) {
+      if (todo.id === id) {
+        todo.editable = true;
+      }
+    }
     this.setState(st => ({ todos: st.todos }));
+  }
+
+  saveTodoEdits(editedTodo) {
+    let editMe = { ...editedTodo };
+
+    let editedTodos = this.state.todos.map(todo => {
+      if (editedTodo.dataId === todo.id) {
+        todo.value = editedTodo.value;
+        todo.editable = false;
+        return todo;
+      } else {
+        return todo;
+      }
+    })
+
+    this.setState(st => ({
+      todos: st.todos
+    }));
   }
 
   render() {
@@ -29,11 +58,16 @@ class TodoList extends Component {
       <div className="TodoList">
         {this.state.todos.map(todo => (
           <React.Fragment>
-            <Todo
-              key={todo.id}
-              value={todo.value}
-              handleDeleteTodo={evt => this.removeTodo(todo.id)}
-            />
+            {!todo.editable ?
+              (<Todo
+                key={todo.id}
+                value={todo.value}
+                editable={false}
+                enableEditTodo={evt => this.makeTodoEditable(todo.id)}
+                handleDeleteTodo={evt => this.removeTodo(todo.id)}
+              />) :
+              (<EditTodoForm dataId={todo.id} currentTodoValue={todo.value} handleEditTodo={this.saveTodoEdits} />)
+            }
             <br />
           </React.Fragment>
         ))}
